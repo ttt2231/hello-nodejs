750d4d92350e6f3dc77fee6159080f2473e1f5a7
ttt2231 <hoangtri2231@gmail.com>
2024-07-19 08:01:21 +0000

    change route

---
+++ index.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var app = express();
+app.get('/about', function (req, res){
+    res.send('Hello world');
+});
+app.listen(3000, function (){
+    console.log('Example app listening on port 3000');
+});
